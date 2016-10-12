29d6a65242f3dc2a60b199d5e0f99494ec80566a
wynhsu <wynhsu@uw.edu>
2016-10-11 18:51:22 -0700

    salaries

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,43 +1,48 @@
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100)
-
+employees <- c(paste("Employee", 1:100, sep=" "))
 
 # Create a vector of 2014 salaries using the runif function
-
+salaries.2014 <- c(runif(100, min=60000, max=120000))
 
 # Create a vector of 2015 salaries that are typically higher than the 2014 salaires (use runif again)
-
+salaries.2015 <- c(runif(100, min=80000, max=160000))
 
 # Create a data.frame 'salaries' by combining the vectors you just made
-
+salaries <- data.frame(employees, salaries.2014, salaries.2015)
 
 # Create a column 'raise' that stores the size of the raise between 2014 and 2015
-
+salaries$raise <- salaries.2015 - salaries.2014
 
 # Create a column 'got.raise' that is TRUE if the person got a raise
-
+salaries$got.raise <- c(salaries.2015 > salaries.2014)
 
 # Retrieve values from your data frame to answer the following questions:
 
 # What was the 2015 salary of employee 57
-
+salaries.2015[57]
+84735.48
 
 # How many employees got a raise?
-
+nrow(salaries[salaries$got.raise == TRUE,])
+76
 
 # What was the value of the highest raise?
-
+max(salaries$raise)
+95520.79
 
 # What was the name of the employee who recieved the highest raise?
-
+Employee 93
 
 # What was the largest decrease in salaries between the two years?
-
+min(salaries.2015 - salaries.2014)
+-31441.17
 
 # What was the name of the employee who recieved largest decrease in salary?
-
+Employee 57
 
 # What was the average salary increase?
-
+mean(salaries$raise)
+30795.95
 
 ### Bonus ###
 
